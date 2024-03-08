7a70dc43be20d4b339de6377691f401aa76f28e5
sanket_25_03_ <109816069+sanket96s@users.noreply.github.com>
2024-03-08 23:07:33 +0530

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -41,7 +41,7 @@ void display()
 {
     i = 0;
     temp = head;
-    printf("\n Data stored is : \n");
+    printf("\n Nodes data stored is : \n");
     while (i < count)
     {
         printf("%d  ", temp->data);
@@ -55,11 +55,11 @@ void add()
 {
     struct node *nn;
     nn = (struct node *)malloc(sizeof(struct node));
-    int p;
+    int position;
     i = 1;
     printf("\n Enter the position to insert data : ");
-    scanf("%d", &p);
-    if (p > count + 1)
+    scanf("%d", &position);
+    if (position > count + 1)
     {
         printf("\n wrong position ");
     }
@@ -68,13 +68,13 @@ void add()
         printf("\n Enter data to insert : ");
         scanf("%d", &nn->data);
         nn->next = 0;
-        if (p == 1)
+        if (position == 1)
         {
             nn->next = head;
             head = nn;
             count++;
         }
-        else if (p == count + 1)
+        else if (position == count + 1)
         {
             temp = head;
             while (temp->next != 0)
@@ -88,7 +88,7 @@ void add()
         else
         {
             temp = head;
-            while (i < p - 1)
+            while (i < position - 1)
             {
                 temp = temp->next;
                 i++;
@@ -104,17 +104,17 @@ void add()
 void delete()
 {
     struct node *Delete;
-    int p;
+    int position;
     i = 1;
     printf("\n Enter the position to delete data : ");
-    scanf("%d", &p);
-    if (p > count)
+    scanf("%d", &position);
+    if (position > count)
     {
         printf("\n wrong position ");
     }
     else
     {
-        if (p == 1)
+        if (position == 1)
         {
             Delete = head;
             head = Delete->next;
@@ -124,7 +124,7 @@ void delete()
         else
         {
             temp = head;
-            while (i < p - 1)
+            while (i < position - 1)
             {
                 temp = temp->next;
                 i++;
@@ -141,17 +141,17 @@ void delete()
 // Main function
 void main()
 {
-    int n, a = 0, choice;
+    int noOfNodes, createdNode = 0, choice;
     printf("\n Enter how many nodes you want to create : ");
-    scanf("%d", &n);
+    scanf("%d", &noOfNodes);
     // Creating nodes
-    while (a < n)
+    while (createdNode < noOfNodes)
     {
         create();
-        a++;
+        createdNode++;
     }
     // Menu-driven loop for performing linked list operations
-    while (a != 0)
+    while (1)
     {
         printf("\n");
         printf("\n 1.add \n 2.delete \n 3.display \n 4.no.of_nodes \n 5.end_program");
@@ -176,7 +176,7 @@ void main()
             break;
 
         case 5:
-            a = 0;
+            return 0;
             break;
 
         default:
