ec61afac4c460ce86c524d7741465fd0276c385f
sanket_25_03_ <109816069+sanket96s@users.noreply.github.com>
2024-03-08 22:42:45 +0530

    Update array_operations.c

--- array_operations.c
+++ array_operations.c
@@ -4,34 +4,44 @@
 #include <stdio.h>
 
 // Function to display elements of the array
-void display(int *a, int no_elements) {
+void display(int *array, int size)
+{
     int i;
     printf("\n The elements are: ");
-    for (i = 0; i < no_elements; i++) {
-        printf("%d ", a[i]);
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
     }
 }
 
 // Function to insert an element into the array
-void insert(int *a, int size, int *no_elements) {
+void insert(int *array, int size, int *no_elements)
+{
     // Check if the array is full
-    if (*no_elements == size) {
+    if (*no_elements == size)
+    {
         printf("\n Array is full");
-    } else {
-        int data, p;
+    }
+    else
+    {
+        int data, i, position;
         printf("\n Enter index to insert data: ");
-        scanf("%d", &p);
+        scanf("%d", &position);
         // Check if the index is within bounds
-        if (p >= size || p < 0) {
+        if (position >= size || position < 0)
+        {
             printf("\n Out of bound position");
-        } else {
+        }
+        else
+        {
             printf("\n Enter data to insert: ");
             scanf("%d", &data);
             // Shift elements to make space for the new element
-            for (int i = *no_elements - 1; i >= p; i--) {
-                a[i + 1] = a[i];
+            for (i = *no_elements - 1; i >= position; i--)
+            {
+                array[i + 1] = array[i];
             }
-            a[p] = data;
+            array[position] = data;
             (*no_elements)++;
             printf("\n Element added successfully");
         }
@@ -39,60 +49,76 @@ void insert(int *a, int size, int *no_elements) {
 }
 
 // Function to delete an element from the array
-void delete (int *a, int size, int *no_elements) {
+void delete(int *array, int size, int *no_elements)
+{
     // Check if the array is empty
-    if (*no_elements == 0) {
+    if (*no_elements == 0)
+    {
         printf("\n Array is empty");
-    } else {
-        int p;
+    }
+    else
+    {
+        int position;
         printf("\n Enter index to delete data: ");
-        scanf("%d", &p);
+        scanf("%d", &position);
         // Check if the index is within bounds
-        if (p >= *no_elements || p < 0) {
+        if (position >= *no_elements || position < 0)
+        {
             printf("\n Out of bound position");
-        } else {
+        }
+        else
+        {
             // Shift elements to overwrite the deleted element
-            for (int i = p; i < *no_elements - 1; i++) {
-                a[i] = a[i + 1];
+            for (int i = position; i < size - 1; i++)
+            {
+                array[i] = array[i + 1];
             }
+            array[size - 1] = 0;
             (*no_elements)--;
             printf("\n Element deleted successfully");
         }
     }
 }
 
-int main() {
-    int size, no_elements;
+int main()
+{
+    int i, size, no_elements, choice;
     printf("\n Enter size of array: ");
     scanf("%d", &size);
-    int a[size];
+    int array[size];
     printf("\n Enter how many elements you want to add: ");
     scanf("%d", &no_elements);
     printf("\n Enter elements of array: ");
-    for (int i = 0; i < no_elements; i++) {
-        scanf("%d", &a[i]);
+    for (i = 0; i < no_elements; i++)
+    {
+        scanf("%d", &array[i]);
     }
-    int ch;
+    for (i = no_elements; i < size; i++)
+    {
+        array[i] = 0;
+    }
     // Menu-driven loop for performing array operations
-    while (1) {
-        printf("\n 1. Insert \t 2. Display \t 3. Delete \t 4. Exit");
+    while (1)
+    {
+        printf("\n 1. Insert \n 2. Delete \n 3. Display \n 4. Exit");
         printf("\n Enter your choice: ");
-        scanf("%d", &ch);
-        switch (ch) {
-            case 1:
-                insert(a, size, &no_elements);
-                break;
-            case 2:
-                display(a, no_elements);
-                break;
-            case 3:
-                delete (a, size, &no_elements);
-                break;
-            case 4:
-                // Exit the program
-                return 0;
-            default:
-                printf("\n Wrong choice");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insert(array, size, &no_elements);
+            break;
+        case 2:
+            delete (array, size, &no_elements);
+            break;
+        case 3:
+            display(array, size);
+            break;
+        case 4:
+            // Exit the program
+            return 0;
+        default:
+            printf("\n Wrong choice");
         }
     }
     return 0;
