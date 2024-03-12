0c382c72b4108eb0ac087d9b39566fbeb102a73a
sanket_25_03_ <109816069+sanket96s@users.noreply.github.com>
2024-03-12 08:18:47 +0530

    Update and rename stack_using_array.c to stackUsingArray.c

--- stackUsingArray.c
+++ stackUsingArray.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int i, size, top = -1;
 
+// Function to push an element onto the stack
 void push(int *stack)
 {
-
+    // Checking if the stack is full
     if (top == size - 1)
     {
         printf("\n Stack is overflow");
@@ -12,46 +14,57 @@ void push(int *stack)
     else
     {
         top++;
+        // Reading data from the user and pushing onto the stack
         printf("\n Enter data : ");
         scanf("%d", &stack[top]);
+        printf("\n Data inserted successfully");
     }
 }
 
+// Function to pop an element from the stack
 void pop(int *stack)
 {
+    // Checking if the stack is empty
     if (top == -1)
     {
-        printf("\n Stack is empty");
+        printf("\n Stack is underflow");
     }
     else
     {
+        // Popping the top element from the stack
         printf("\n %d deleted", stack[top]);
         top--;
     }
 }
 
+// Function to peek at the top element of the stack
 void peak(int *stack)
 {
+    // Checking if the stack is empty
     if (top == -1)
     {
-        printf("\n Stack is empty");
+        printf("\n Stack is underflow");
     }
     else
     {
+        // Displaying the top element of the stack
         printf("\n Top element is : %d", stack[top]);
     }
 }
 
+// Function to display all elements of the stack
 void display(int *stack)
 {
     int count = top;
+    // Checking if the stack is empty
     if (count == -1)
     {
-        printf("\n Stack is empty");
+        printf("\n Stack is underflow");
     }
     else
     {
         printf("\n Data stored is : ");
+        // Displaying elements from top to bottom of the stack
         while (count != -1)
         {
             printf("%d ", stack[count]);
@@ -60,39 +73,47 @@ void display(int *stack)
     }
 }
 
-void main()
+int main()
 {
-
     int choice;
+    // Getting the size of the stack from the user
     printf("\n Enter size of stack : ");
     scanf("%d", &size);
+    // Declaring the stack array
     int stack[size];
+    // Menu-driven loop for stack operations
     while (1)
     {
-        printf("\n 1.push \t 2.pop \t 3.peak \t 4.display");
+        printf("\n");
+        printf("\n 1.push \n 2.pop \n 3.peak \n 4.display \n 5.exit");
         printf("\n Enter your choice : ");
         scanf("%d", &choice);
+        // Performing operations based on user choice
         switch (choice)
         {
         case 1:
-            push(&stack);
+            push(stack);
             break;
 
         case 2:
-            pop(&stack);
+            pop(stack);
             break;
 
         case 3:
-            peak(&stack);
+            peak(stack);
             break;
 
         case 4:
-            display(&stack);
+            display(stack);
             break;
+
+        case 5:
+            return 0;
 
         default:
             printf("wrong choice");
             break;
         }
     }
+    return 0;
 }
